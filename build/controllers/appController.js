4287208cb5c4720247a2998cb2a2c64ad3bfd36c
Samuh-El <samuelgajardo@outlook.com>
2020-04-24 14:58:09 -0400

    xd

--- build/controllers/appController.js
+++ build/controllers/appController.js
@@ -178,9 +178,6 @@ class AppController {
     //obtener usuario-administrador en panel, retorna los datos del usuario y el nombre de la pyme asociada, requiere el id del usuario
     getUsuario(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            return res.json({
-                text: "paso la vola xd"
-            });
 
             console.log('getusuario metodo en node');
             const usuario = yield database_1.default.query('SELECT u.NombreUsuario,u.ApellidoUsuario,u.celular,u.correo,u.direccion,p.nombrePyme FROM `Usuario-Administrador` AS u INNER JOIN `pyme` AS p ON u.Pyme_idPyme = p.idPyme where u.idUsuario = ?', [req.params.id]);
