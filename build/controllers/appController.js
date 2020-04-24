5d55ad48ce9a84d4314c0d671fb40db6403dbbd2
Samuh-El <samuelgajardo@outlook.com>
2020-04-24 17:05:06 -0400

    bla6

--- build/controllers/appController.js
+++ build/controllers/appController.js
@@ -163,7 +163,7 @@ class AppController {
             //     )
             //     return    
                 const token = jsonwebtoken_1.default.sign({ _id: Admin.idUsuario }, 'secretkey');
-               res.json(token)
+                return res.status(200).json({ Admin, token });
             //    return res.json({ Admin, token })
 
                 
