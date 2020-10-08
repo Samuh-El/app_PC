40898d9803736ee0eff9ce24d4159deba7a0ae16
Samuh-El <62481894+Samuh-El@users.noreply.github.com>
2020-10-08 18:32:17 -0300

    Add files via upload

---
+++ keys.ts
@@ -0,0 +1,20 @@
+export default{
+     database:{
+
+          //datos de conexion a db
+
+          host:'productochile.cl',
+          user:'producto_Felipe',  
+          password:']wq-8xIFvf26',
+          database:'producto_chile',   
+           
+ 
+          //datos de conexion a localhost
+          
+          // host:'localhost', 
+          // user:'root', 
+          // password:'',
+          // database:'productoschile',
+     }   
+      
+}
